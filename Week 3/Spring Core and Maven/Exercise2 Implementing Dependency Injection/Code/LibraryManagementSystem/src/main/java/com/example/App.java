712e20abc563dc1cf6962af339cab42b58817489
Tanu Choudhary <tanuchoudhary@Tanus-MacBook.local>
2025-07-06 18:28:31 +0530

    Add folder to main branch

---
+++ Week 3/Spring Core and Maven/Exercise2 Implementing Dependency Injection/Code/LibraryManagementSystem/src/main/java/com/example/App.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import com.example.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+// ID: 6386182
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Application Started");
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService service = context.getBean(BookService.class);
+
+        service.printBookRepository();
+    }
+}
